b257a519734d39eac20ae50618fca5fb4d5899f3
David Bernhard <davidbernhard@live.com>
2019-03-22 19:23:32 +0000

    set up context

--- src/components/tracks/Tracks.js
+++ src/components/tracks/Tracks.js
@@ -1,8 +1,16 @@
 import React, { Component } from "react";
+import { Consumer } from "../../context";
 
 class Tracks extends Component {
   render() {
-    return <h1>Tracks</h1>;
+    return (
+      <Consumer>
+        {value => {
+          console.log(value);
+          return <h1>Tracks</h1>;
+        }}
+      </Consumer>
+    );
   }
 }
 
