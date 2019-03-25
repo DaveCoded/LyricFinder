941c74d033e49d0da723bd559a5ebd18bfcbf4e2
David Bernhard <davidbernhard@live.com>
2019-03-25 10:53:18 +0000

    Updates

---
+++ precache-manifest.791cdd3ed8af520c41620a70e7a58d01.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "bd2ba84e908d4d365a53",
+    "url": "/LyricFinder/static/js/runtime~main.616980da.js"
+  },
+  {
+    "revision": "b3f9e7e5b30d10655e8e",
+    "url": "/LyricFinder/static/js/main.893cee39.chunk.js"
+  },
+  {
+    "revision": "1e8c68849af5f3c1bba1",
+    "url": "/LyricFinder/static/js/2.a519bad8.chunk.js"
+  },
+  {
+    "revision": "b3f9e7e5b30d10655e8e",
+    "url": "/LyricFinder/static/css/main.11d832ee.chunk.css"
+  },
+  {
+    "revision": "6f17080b92ee4bac4430ab0fc876ab54",
+    "url": "/LyricFinder/index.html"
+  }
+];
